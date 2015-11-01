29496edc432cbc69923c34cb1c7aa88d37edecea
Peter B Smith <peterbutlersmith@gmail.com>
2015-10-31 21:00:29 -0400

    boilerplate serverside authorization

--- src/server.js
+++ src/server.js
@@ -6,18 +6,39 @@ var morgan     = require("morgan");
 var passport   = require("passport");
 var ejs        = require("ejs");
 var bodyParser = require("body-parser");
+var session    = require('express-session');
+var db         = require('./server/config/database.js');
+
 var port = process.env.PORT || 8080;
 
 // ---- serve up static files
 app.use(express.static(__dirname + "/client/"));
 
-
+// ---- use middleware
 app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// ---- connect to dbs
+mongoose.connect(db.mongo.url);
+
+
+// ---- set view engine
 app.set('views', './src/server/views');
 app.set('view engine', 'ejs'); // set up ejs for templating
 
+
+// ---- configure passport
+require('./server/config/passport')(passport);
+app.use(session({ secret: "thisIsSuperDuperUBERsecret", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// --- establish auth router
+require('./server/routes/auth')(app,passport);
+
+
+// ---- serve entry routes
 app.get('/',function(req,res){
   res.render('index.ejs');
 });
@@ -26,6 +47,8 @@ app.get('*',function(req,res){
   res.render('index.ejs');
 });
 
+
+// ---- start server
 app.listen(port,function(){
   console.log("Running on port " + port);
 });
