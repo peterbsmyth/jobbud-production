9a6f31a366c91dc91b6250502ae3d3ad4d1f142b
Peter B Smith <peterbutlersmith@gmail.com>
2015-11-02 11:01:48 -0500

    angular signup

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -38,22 +38,4 @@ var jobApp = angular.module('jobApp',['ui.router','ngResource','ngCookies'])
       enabled: true,
       requireBase: false // problematic
     });
-  })
-
-  .run(function ($rootScope, $location, Auth) {
-
-    //watching the value of the currentUser variable.
-    $rootScope.$watch('currentUser', function(currentUser) {
-      // if no currentUser and on a page that requires authorization then try to update it
-      // will trigger 401s if user does not have a valid session
-      if (!currentUser && (['/', '/login', '/logout', '/signup'].indexOf($location.path()) == -1 )) {
-        Auth.currentUser();
-      }
-    });
-
-    // On catching 401 errors, redirect to the login page.
-    $rootScope.$on('event:auth-loginRequired', function() {
-      $location.path('/login');
-      return false;
-    });
   });
--- src/client/js/controllers/SignupCtrl.js
+++ src/client/js/controllers/SignupCtrl.js
@@ -1,5 +1,19 @@
-jobApp.controller('SignupCtrl',['$scope',
-  function($scope){
+jobApp.controller('SignupCtrl',['$scope','$rootScope','$location','$resource','User','Auth',
+  function($scope,$rootScope,$location,$resource,User,Auth){
 
+    $scope.signup = function(){
+
+      Auth.createUser({
+        email: $scope.user.email,
+        password: $scope.user.password
+      },
+      function(err){
+          $scope.errors = {};
+
+          if(!err){
+            $location.path('/seeker');
+          }
+      });
+    };
   }
 ]);
