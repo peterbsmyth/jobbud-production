fb41a30d8caa8e1e50f41cceb3b9f2a7d456bca6
Peter B Smith <peterbutlersmith@gmail.com>
2015-11-02 16:04:17 -0500

    added userInfo virtual

--- src/server/models/user.js
+++ src/server/models/user.js
@@ -14,16 +14,25 @@ var userSchema = mongoose.Schema({
     }
 });
 
+// ---- virtuals
+userSchema.virtual('userInfo')
+  .get(function (){
+    return {'_id': this._id, 'email': this.local.email};
+  });
+
+
 // ---- methods ----
 // ---- generating a hash
 userSchema.methods.generateHash = function(password) {
     return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
 };
 
+
 // ---- checking if password is valid
 userSchema.methods.validPassword = function(password) {
     return bcrypt.compareSync(password, this.local.password);
 };
 
-// ---- create the model for users and expose it to our app
+
+// ---- create the model for users and expose it
 module.exports = mongoose.model('User', userSchema);
